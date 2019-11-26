a0ac1da1f135b5f87e953007b240aed74e93cdce
K3LV1NL33 <kelvinahke@gmail.com>
2019-11-26 18:21:04 +0800

    Index update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var restaurant = require('./restaurant_api');
+
+// Retrieve and return all notes from the database.
+exports.findAll = function (req, callback) {
+  req.db.collection("restaurant").find({}).toArray(function (err, res2) {
+      if (err) throw err;
+      callback(res2);
+  });
+};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  exports.findAll(req, function (restaurant_array) {
+    //res.json(restaurant_array);
+    res.render('index', { title: 'Restaurants Collection',restaurants: restaurant_array });
+  });
 });
 
 module.exports = router;
